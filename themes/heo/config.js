17a09cd4b390593ac5d791f23f258bc2a8b4b638
Luca Liu <69256589+luca1iu@users.noreply.github.com>
2025-08-06 14:37:21 +0200

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,17 +21,17 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: 'Data Analysis',
   HEO_HERO_TITLE_2: 'and Web Development',
-  HEO_HERO_TITLE_3: 'luca-liu.COM',
+  HEO_HERO_TITLE_3: 'luca-liu.com',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  HEO_HERO_COVER_TITLE: 'Walk Around',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: 'Must-Read Picks', url: '/tag/Must-Read Picks' },
   HEO_HERO_CATEGORY_2: { title: 'Popular Articles', url: '/tag/Popular Articles' },
-  HEO_HERO_CATEGORY_3: { title: 'Tutorials', url: '/tag/Tutorials' },
+  HEO_HERO_CATEGORY_3: { title: 'Practical Tutorials', url: '/tag/Practical Tutorials' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,7 +40,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    'Hi！I am',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
@@ -52,10 +52,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/luca1iu',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL3: 'https://www.luca-liu.com'
+
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
