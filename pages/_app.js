942c46ad20fc88caf239da9ccc299182fb524cb1
luca1iu <lucaliu217@outlook.com>
2025-08-09 18:59:20 +0200

    font5

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ import '@/styles/devto-fonts.css' // DEV.to 风格字体
 // core styles shared by all of react-notion-x (required)
 import '@/styles/notion.css' //  重写部分notion样式
 import 'react-notion-x/src/styles.css' // 原版的react-notion-x
+import '@/styles/inline-code-override.css' // 内联代码样式强制覆盖
+import '@/styles/gruvbox-override.css' // Gruvbox主题强制覆盖
 
 import useAdjustStyle from '@/hooks/useAdjustStyle'
 import { GlobalContextProvider } from '@/lib/global'
