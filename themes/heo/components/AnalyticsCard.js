991f7819e544ff1dd883dcea35dee96708da65cb
luca1iu <lucaliu217@outlook.com>
2025-08-09 12:46:02 +0200

    days

--- themes/heo/components/AnalyticsCard.js
+++ themes/heo/components/AnalyticsCard.js
@@ -28,7 +28,7 @@ export function AnalyticsCard(props) {
             <div className='inline'>
                 <div className='flex justify-between'>
                     <div>{siteTimeTitle}</div>
-                    <div>{diffDays} 天</div>
+                    <div>{diffDays} Days</div>
                 </div>
             </div>
             <div className='hidden busuanzi_container_page_pv'>
